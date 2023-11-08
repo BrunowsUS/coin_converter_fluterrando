ce91c97cab5db7f6eb16b157d8c14e8c03cdaddd
BuildTools <unconfigured@null.spigotmc.org>
2023-11-08 20:57:28 -0300

    aula31_completed

--- lib/app/views/HomeView.dart
+++ lib/app/views/HomeView.dart
@@ -1,3 +1,4 @@
+import 'package:coin_converter/app/models/currency_model.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,45 +7,73 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          SizedBox(
-            height: 50,
-            width: 50,
-          ),
-          Column(children: [
-            Image.asset(
-              'assets/currency_logo.png',
-              width: 150,
-              height: 150,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(children: [
+                Image.asset(
+                  'assets/currency_logo.png',
+                  width: 150,
+                  height: 150,
+                ),
+                SizedBox(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: SizedBox(
+                            height: 55,
+                            child: DropdownButton(
+                                isExpanded: true,
+                                underline: Container(
+                                  height: 1,
+                                  color: Colors.amber,
+                                ),
+                                items: [
+                                  DropdownMenuItem(
+                                      value: (), child: Text('Dolar')),
+                                  DropdownMenuItem(
+                                      value: (), child: Text('Euro')),
+                                ],
+                                onChanged: (value) {}),
+                          )),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Expanded(
+                          flex: 2,
+                          child: TextField(
+                            decoration: InputDecoration(),
+                          ))
+                    ],
+                  ),
+                ),
+              ]),
             ),
             SizedBox(
-              child: Row(
-                children: [
-                  DropdownButton(items: [], onChanged: (value) {}),
-                  Text('teste')
-                ],
-              ),
+              height: 20,
+              width: 20,
             ),
-          ]),
-          SizedBox(
-            height: 20,
-            width: 20,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 20,
-                width: 80,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('teste'),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 20,
+                  width: 100,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Converter'),
+                  ),
                 ),
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
